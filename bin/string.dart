19c023d3dc8d1eeaa01b448363839cd56f8ccefd
Rahmat Hidayat <me.rahmat.hidayat@gmail.com>
2023-03-30 14:14:52 +0800

    string, interpolation, backslash, concat, longstr

---
+++ bin/string.dart
@@ -0,0 +1,21 @@
+void main() {
+  String me = 'mamato';
+  print(me);
+
+  me = 'ok';
+  print(me);
+
+  print('halo $me \$');
+
+  String first = 'mama';
+
+  String last = 'toto';
+
+  print(first + last);
+
+  String halo = '''
+remark 
+pangjang
+''';
+  print(halo);
+}
