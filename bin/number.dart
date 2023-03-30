17cd6833d19d1b94eda447b7b4e520a463d6db31
Rahmat Hidayat <me.rahmat.hidayat@gmail.com>
2023-03-30 10:14:36 +0800

    belajar number - int, double, num

---
+++ bin/number.dart
@@ -0,0 +1,13 @@
+void main() {
+  int number1 = 100;
+  double number2 = 200.5;
+
+  print(number1);
+  print(number2);
+
+  num number3 = 10;
+  print(number3);
+
+  number3 = 12.3;
+  print(number3);
+}
