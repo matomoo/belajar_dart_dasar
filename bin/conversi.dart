dbc07f3030c7b9b86ec3439812477ce485834baa
Rahmat Hidayat <me.rahmat.hidayat@gmail.com>
2023-03-31 04:37:00 +0800

    conversi

---
+++ bin/conversi.dart
@@ -0,0 +1,11 @@
+void main() {
+  var sss = '1000';
+  var aaa = int.parse(sss);
+  var zzz = double.parse(sss);
+
+  var az = aaa.toDouble();
+  var za = zzz.toInt();
+
+  var as = aaa.toString();
+  var zs = zzz.toString();
+}
