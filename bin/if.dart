de843398415b947cbfc76c4216400d62d5a202c6
Rahmat Hidayat <me.rahmat.hidayat@gmail.com>
2023-04-01 18:02:32 +0800

    switch

---
+++ bin/if.dart
@@ -0,0 +1,12 @@
+void main() {
+  var n1 = 70;
+  var n2 = 90;
+
+  if (n1 > 75 && n2 > 75) {
+    print('Lulus');
+  } else if (n1 > 65 && n2 > 65) {
+    print('Hampir lulus');
+  } else {
+    print('tidak lulus');
+  }
+}
