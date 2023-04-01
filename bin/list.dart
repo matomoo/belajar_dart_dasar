3bcc3d714718f76f7a64f04e49f5ac116c4936f9
Rahmat Hidayat <me.rahmat.hidayat@gmail.com>
2023-04-01 17:29:35 +0800

    list

---
+++ bin/list.dart
@@ -0,0 +1,15 @@
+void main() {
+  List<int> aaa = [];
+
+  var bbb = <String>[];
+  final ccc = <String>[];
+
+  aaa.add(1);
+  aaa.add(10);
+
+  print(aaa);
+  print(aaa.length);
+
+  var ddd = [1, 2, 3, 4, 5];
+  print(ddd);
+}
