cff5130cf87a13ed3185fc61c0a682ecd2ec2b65
Rahmat Hidayat <me.rahmat.hidayat@gmail.com>
2023-04-01 18:11:46 +0800

    switch

---
+++ bin/switch.dart
@@ -0,0 +1,16 @@
+void main() {
+  var n1 = 'B';
+
+  switch (n1) {
+    case 'A':
+      print("Lulus");
+      break;
+    case 'B':
+
+    case 'D':
+      print("Tidak lulus");
+      break;
+    default:
+      print('Beljar lagi');
+  }
+}
